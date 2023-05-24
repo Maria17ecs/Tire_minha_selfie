1ed0c40eaa4842db72184e68ff1bef4d7e9e8a10
Maria17ecs <122824717+Maria17ecs@users.noreply.github.com>
2023-05-24 18:32:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var Content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-      if(Content =="Tire minha selfie.")
+      if(Content =="tire minha selfie")
       {
         console.log("tirando selfie --- ");
         speak();
